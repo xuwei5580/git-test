49e759212f4c93525ff20adfaa51ae2e74203cbb
xuwei <xuwei5580@163.com>
2021-08-04 20:15:34 +0800

    fourth commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello git2");
         System.out.println("hello git3");
+        System.out.println("hello xuwei5580@163.com");
     }
 }
